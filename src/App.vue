438c9df055b59c1f0d5e0cae255dd5c56911ec30
Wenyu520521 <2584107537@qq.com>
2023-05-05 10:32:35 +0800

    element-ui,表单校验

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 卡片组件 -->
+    <el-card class="login-card">
+      <!-- 登录表单 -->
+      <el-form
+        style="margin-top: 50px"
+        :model="loginForm"
+        :rules="loginRules"
+        ref="loginForm"
+      >
+        <el-form-item prop="mobile">
+          <el-input
+            placeholder="请输入手机号"
+            v-model="loginForm.mobile"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            placeholder="请输入密码"
+            v-model="loginForm.password"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width: 100%" @click="login"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    // 自定义校验函数
+    const checkMobile = (rule, value, callback) => {
+      value.charAt(2) === '9'
+        ? callback()
+        : callback(new Error('第三位手机号必须为9'))
+    }
+    return {
+      // 定义表单数据对象
+      loginForm: {
+        mobile: '',
+        password: '',
+      },
+
+      // 校验规则
+      loginRules: {
+        // 校验的字段名
+        mobile: [
+          {
+            required: true,
+            message: '手机号不能为空',
+            trigger: 'blur',
+          },
+          {
+            pattern: /^1[3-9]\d{9}$/,
+            message: '请输入正确的手机号',
+            trigger: 'blur',
+          },
+          {
+            trigger: 'blur',
+            validator: checkMobile,
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: '密码不能为空',
+            trigger: 'blur',
+          },
+          {
+            min: 6,
+            max: 16,
+            message: '密码应为6-16位的长度',
+            trigger: 'blur',
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    login() {
+      this.$refs.loginForm.validate((isOk) => {
+        if (isOk) {
+          console.log('检验通过') // 执行校验通过的业务
+          return
+        }
+        console.log('校验失败')
+      })
+    },
+  },
+  components: {},
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100vh;
+  background-color: pink;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-card {
+  width: 440px;
+  height: 300px;
 }
 </style>
